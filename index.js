602e0e52733f43ac8a666bc25cfd8d843d39fc3c
Serhii T <tocarchuk@gmail.com>
2021-11-01 13:32:30 +0200

    Validate results and display winner

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ let currentPlayer = 'X';
 let isGameActive = true;
 
 const marks = Array.from(document.querySelectorAll('.mark'));
+const announcer = document.getElementById('announce-result');
 
 marks.forEach( (mark, index) => {
   mark.addEventListener('click', () => userAction(mark, index));
 })
 
 function userAction(mark, index){
-  mark.innerText = currentPlayer;
-  mark.classList.add(`player${currentPlayer}`);
-  saveBoardIndex(index);
-  changePlayer();
+  if( isGameActive ){
+    mark.innerText = currentPlayer;
+    mark.classList.add(`player${currentPlayer}`);
+    saveBoardIndex(index);
+    resultValidation();
+    changePlayer();
+  }
 }
 
 function saveBoardIndex(index){
@@ -21,6 +25,38 @@ function saveBoardIndex(index){
 
 function changePlayer(){
   currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function resultValidation(){
+  let roundWon = false;
+  for( let i = 0; i <= 7; i++ ){
+    const winCondition = winningConditions[i];
+    const a = board[winCondition[0]];
+    const b = board[winCondition[1]];
+    const c = board[winCondition[2]];
+    if( a === '' || b === '' || c === '' ){
+      continue;
+    }
+    if( a === b && b === c ){
+      roundWon = true;
+      break;
+    }
+  }
+  if( roundWon ){
+    announceResult(currentPlayer === 'X' ? 'X' : 'O');
+    isGameActive = false;
+    return;
+  }
+}
+
+function announceResult(result){
+  if( result ===  'X' ){
+    announcer.innerHTML = `Player <span class="playerX">X</span> Won`;
+  } else if (result === 'O' ){
+    announcer.innerHTML = `Player <span class="playerO">O</span> Won`;
+  } else {
+    announcer.innerText = 'Tie';
+  }
 }
 
 // Indexes within the board
